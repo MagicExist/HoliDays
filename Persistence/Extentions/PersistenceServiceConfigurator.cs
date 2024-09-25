e0f9890153d572d4addbfc8f5111fe411353a6a6
MagicExist <johhanparra1@gmail.com>
2024-09-25 11:11:35 -0500

    Add project files.

---
+++ Persistence/Extentions/PersistenceServiceConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using HoliDays.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace Persistence.Extentions
+{
+    public static class PersistenceServiceConfigurator
+    {
+        public static IServiceCollection PersistenceService(this IServiceCollection service, IConfiguration configuration)
+        {
+            service.AddDbContext<FestivosContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("Default")));
+            return service;
+        }
+    }
+}
