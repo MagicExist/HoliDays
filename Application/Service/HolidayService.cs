a134bf15e3eb8b73681c185758cd9bb758a99a44
MagicExist <johhanparra1@gmail.com>
2024-09-25 13:28:55 -0500

    HolidayService Implemented

---
+++ Application/Service/HolidayService.cs
@@ -0,0 +1,25 @@
+﻿
+using Domain.Repository;
+using HoliDays.Models;
+
+namespace Application.Service
+{
+    public class HolidayService
+    {
+        private readonly IHoliDays _holiDayRepository;
+        public HolidayService(IHoliDays holiDayRepository)
+        {
+            _holiDayRepository = holiDayRepository;
+        }
+
+        public async Task<IEnumerable<Festivo>> GetHolyDaysAsync(int year)
+        {
+            return await _holiDayRepository.GetHolyDaysAsync(year);
+        }
+
+        public async Task<bool> IsHolyDay(DateTime date)
+        {
+            return await _holiDayRepository.IsHolyDay(date);
+        }
+    }
+}
