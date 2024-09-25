dfc5998f0f9df6ba02c1be55380ce1792afe670c
target-id <codeberrio@gmail.com>
2024-09-25 12:52:51 -0500

    Method added "GetHolidaysAsync"

---
+++ Persistence/Repositories/HolyDay.cs
@@ -0,0 +1,71 @@
+﻿using Domain.Repository;
+using HoliDays.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32.SafeHandles;
+using Persistence.DbHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Repositories
+{
+    public class HolyDay :IHolyDays
+    {
+        private readonly FestivosContext _context;
+
+        public HolyDay(FestivosContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Festivo>> GetHolyDaysAsync(int year)
+        {
+            var holidayList = await _context.Festivos.ToArrayAsync();
+            var sundayEaster = HoliDaysHelper.GetPalmSunday(year).AddDays(7);
+            DateTime currentDate = new DateTime();
+            DateTime holidayBaseOnSundayEaster = new DateTime();
+
+            foreach (var holiday in holidayList) 
+            {
+                switch (holiday.IdTipo)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        currentDate = new DateTime(year, holiday.Mes, holiday.Dia);
+                        currentDate = HoliDaysHelper.NextMonday(currentDate);
+                        holiday.Dia = currentDate.Day;
+                        holiday.Mes = currentDate.Month;
+                        break;
+                    case 3:
+                        holidayBaseOnSundayEaster = new DateTime(year, sundayEaster.Month, sundayEaster.Day);
+                        holidayBaseOnSundayEaster.AddDays(holiday.DiasPascua);
+                        holiday.Dia = holidayBaseOnSundayEaster.Day;
+                        holiday.Mes = holidayBaseOnSundayEaster.Month;
+                        break;
+                    case 4:
+
+                        holidayBaseOnSundayEaster = new DateTime(year, sundayEaster.Month, sundayEaster.Day);
+                        holidayBaseOnSundayEaster.AddDays(holiday.DiasPascua);
+
+                        currentDate = new DateTime(year, holidayBaseOnSundayEaster.Month, holidayBaseOnSundayEaster.Day);
+                        currentDate = HoliDaysHelper.NextMonday(currentDate);
+                        holiday.Dia = currentDate.Day;
+                        holiday.Mes = currentDate.Month;
+                        break;
+                    
+
+                }
+            }
+
+            return holidayList;
+        }
+
+        public Task<bool> IsHolyDay(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
